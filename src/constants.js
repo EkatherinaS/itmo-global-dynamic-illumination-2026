f42e73c825b38b73c43762f9839e5554ff588b08
Ekaterina Semchuk <79217493+EkatherinaS@users.noreply.github.com>
2026-02-16 00:19:40 +0300

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 import * as THREE from "three/webgpu";
 
-export const WIDTH = 16;
-export const HEIGHT = 16;
+export const WIDTH = 4;
+export const HEIGHT = 4;
 
 export const luminanceStorageTexture = new THREE.StorageTexture(WIDTH, HEIGHT);
 export const luminanceStorageCubemap = new THREE.StorageTexture(
@@ -12,4 +12,5 @@ export const irradianceStorageCubemap = new THREE.StorageTexture(
 	4 * WIDTH,
 	3 * HEIGHT,
 );
+
 
